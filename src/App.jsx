18c6888fbf6b39c7592e06d1c478ed31ead4f865
psiddharthdesign <psiddharthdesign@gmail.com>
2022-06-25 20:10:14 +0530

    feat(init): init

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,58 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import styled from 'styled-components'
+
+const StyledParagraph = styled.p`
+border: 1px solid red;
+padding: 16px;
+`
+
+const StyledButton = styled.button`
+  background: dodgerblue;
+  outline: none;
+  border: 1px solid lightblue;
+  transition: all 0.1s;
+  cursor: pointer;
+  &:hover{
+    transform: scale(1.1);
+  }
+`
+
+const RedStyledButton = styled(StyledButton)`
+  background: red;
+`
+
+
+function NameSayer(props){
+  console.log(props);
+  const [myValue, setMyValue] = useState(props.age);
+  const [myOtherValue, setMyOtherValue] = useState(5);
+  const increment = () => {
+    setMyValue(myValue + 1);
+  }
+
+  const decrement = () => {
+    setMyValue(myValue - 1);
+  }
+
+  const increment2 = () => {
+    setMyOtherValue(myOtherValue + 1);
+  }
+  
+  return <div>
+    <StyledParagraph>My name is <span  >{props.name}</span>. My age is {myValue}. My other value is {myOtherValue}</StyledParagraph>
+    <StyledButton onClick={increment}>Increment</StyledButton>
+    <RedStyledButton onClick={decrement} children="Decrement"></RedStyledButton>
+  </div>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+  return <>
+    <NameSayer name='bhargav' age={30}>Hey how are you? </NameSayer>
+    <NameSayer name='siddharth' age={25}/>
+    <NameSayer name='vishwaja' age={27}/>
+  </>;
 }
 
 export default App
